c1f79a9e9d6b02e91a62b70395f00ddc9444fe70
SnehaBirodkar <snehabirodkar31@gmail.com>
2021-04-04 14:39:29 +0530

    designing section layout started

--- assets/script/isotopes.js
+++ assets/script/isotopes.js
@@ -1,35 +1,10 @@
-$(document).ready(function () {
-    $("#isotope-container").isotope({});
-
-    $("#isotope-filters").on("click", "button", function () {
-        let filtervalue = $(this).attr("data-filter");
-        //        console.log(filtervalue);
-
-        $("#isotope-container").isotope({
-            filter: filtervalue
-        });
-        $("#isotope-filters").find('.active').removeClass('active');
-
-        $(this).addClass('active');
-    })
-})
-
-
-$(document).ready(function () {
-    $("#portfolio-wrapper").magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        gallery: {
-            enabled: true
-        },
-        zoom: {
-            enabled: true,
-            duration: 300,
-            easing: 'ease-in-out',
-            opener: function (openerElement) {
-                return openerElement.is('img') ? openerElement : openerElement
-                    .find('img');
-            }
-        }
-    })
-});
+var $btns = $('.btn').click(function() {
+    if (this.id == 'all') {
+      $('#parent > div').fadeIn(450);
+    } else {
+      var $el = $('.' + this.id).fadeIn(450);
+      $('#parent > div').not($el).hide();
+    }
+    $btns.removeClass('active');
+    $(this).addClass('active');
+  })
