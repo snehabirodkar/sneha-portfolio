cd0ce37596a6b7daf28f1b013bbf16f0345a5aa7
SnehaBirodkar <snehabirodkar31@gmail.com>
2021-04-05 14:28:00 +0530

    projectSection icons updates

--- assets/script/script.js
+++ assets/script/script.js
@@ -46,3 +46,11 @@ $('.responsive').slick({
     ]
 })
 
+// Show more
+jQuery(document).ready(function ($) {
+    $(".show-more-btn").click(function (e) {
+      $(".show-more-item:hidden").slice(0).fadeIn();
+      if ($(".show-more-item:hidden").length < 1) $(this).fadeOut();
+    });
+  });
+  
