bf235a579ebc093be37d352d872e61980a30f88e
SnehaBirodkar <snehabirodkar31@gmail.com>
2021-04-06 00:27:17 +0530

    design section partial update

--- assets/script/isotopes.js
+++ assets/script/isotopes.js
@@ -1,10 +1,35 @@
-var $btns = $('.btn').click(function() {
-    if (this.id == 'all') {
-      $('#parent > div').fadeIn(450);
-    } else {
-      var $el = $('.' + this.id).fadeIn(450);
-      $('#parent > div').not($el).hide();
-    }
-    $btns.removeClass('active');
-    $(this).addClass('active');
+$(document).ready(function(){
+  $("#isotope-container").isotope({});
+  
+  $("#isotope-filters").on("click","button",function(){
+      let filterValue=$(this).attr("data-filter");
+//        console.log(filterValue);
+      $("#isotope-container").isotope({
+          filter: filterValue
+      });
+      
+      //active button
+      $("#isotope-filters").find('.active').removeClass('active');
+      $(this).addClass('active');
+  });
+});
+
+$(document).ready(function(){
+  $("#portfolio-wrapper").magnificPopup({
+      delegate:'a',
+      type:'image',
+      gallery:{
+          enabled: true
+  },
+  zoom:{
+      enabled:true,
+      duration:300,
+      easing:'ease-in-out',
+      
+      opener:function(openerElement){
+          return openerElement.is('img') ? openerElement:
+          openerElement.find('img')
+      }
+  }
   })
+});
