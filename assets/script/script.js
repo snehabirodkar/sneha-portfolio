8d5b6d0b9172a6b904a356617f7df498cd37c418
SnehaBirodkar <snehabirodkar31@gmail.com>
2021-04-06 23:05:41 +0530

    testimonial section completed

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,35 +1,37 @@
-var typed = new Typed("#typer", {
-    strings: ['Freelancer',   
-    'UI/UX Designer',
-    'Graphic Designer', 
-    'Full Stack Dev'
-    ],
-    typeSpeed: 120,
-    backSpeed: 40,
-    backDelay: 500,
-    startDelay: 500,
-    smartBackspace: true,
-    loop: true
-});
+// var typed = new Typed("#typer", {
+//     strings: ['Freelancer',   
+//     'UI/UX Designer',
+//     'Graphic Designer', 
+//     'Full Stack Dev'
+//     ],
+//     typeSpeed: 120,
+//     backSpeed: 40,
+//     backDelay: 500,
+//     startDelay: 500,
+//     smartBackspace: true,
+//     loop: true
+// });
 // ***********************************************//
 // social - slick
+
 $('.responsive').slick({
-    // dots: true,
+    dots: false,
     infinite: true,
-    speed: 200,
-    prevArrow:false,
-    nextArrow:false,
+    speed: 300,
     autoplay: true,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
-    // edgeFriction: 0.50,
+    edgeFriction: 0.50,
+    prevArrow: "<button type='button' class='slick-prev'><i class=\"fa fa-angle-left\" aria-hidden=\"true\"></i></button>",
+    nextArrow: "<button type='button' class='slick-next'><i class=\"fa fa-angle-right\" aria-hidden=\"true\"></i></button>",
+    accessibility: false,
     responsive: [{
             breakpoint: 1024,
             settings: {
                 slidesToShow: 3,
                 slidesToScroll: 3,
-                infinite: true
-                // dots: true
+                infinite: true,
+                dots: true
             }
         },
         {
@@ -46,11 +48,3 @@ $('.responsive').slick({
     ]
 })
 
-// Show more
-jQuery(document).ready(function ($) {
-    $(".show-more-btn").click(function (e) {
-      $(".show-more-item:hidden").slice(0).fadeIn();
-      if ($(".show-more-item:hidden").length < 1) $(this).fadeOut();
-    });
-  });
-  
