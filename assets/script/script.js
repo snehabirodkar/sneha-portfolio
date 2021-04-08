7473b434bd49885c430327e6e9665b8d22cfd0b9
SnehaBirodkar <snehabirodkar31@gmail.com>
2021-04-08 21:53:10 +0530

    footer section done

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,16 @@
-// var typed = new Typed("#typer", {
-//     strings: ['Freelancer',   
-//     'UI/UX Designer',
-//     'Graphic Designer', 
-//     'Full Stack Dev'
-//     ],
-//     typeSpeed: 120,
-//     backSpeed: 40,
-//     backDelay: 500,
-//     startDelay: 500,
-//     smartBackspace: true,
-//     loop: true
-// });
+var typed = new Typed("#typer", {
+    strings: ['Freelancer',   
+    'UI/UX Designer',
+    'Graphic Designer', 
+    'Full Stack Dev'
+    ],
+    typeSpeed: 120,
+    backSpeed: 40,
+    backDelay: 500,
+    startDelay: 500,
+    smartBackspace: true,
+    loop: true
+});
 // ***********************************************//
 // social - slick
 
@@ -38,8 +38,8 @@ $('.responsive').slick({
             breakpoint: 600,
             // 600 or less than 600
             settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToShow: 1,
+                slidesToScroll: 1
             }
         }
         // You can unslick at a given breakpoint now by adding:
@@ -48,3 +48,92 @@ $('.responsive').slick({
     ]
 })
 
+// Counter
+$(".number").counterUp({time:3000});
+
+//Load more
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+
+
+
+jQuery(document).ready(function ($) {
+    $(".show-more-btn").click(function (e) {
+        $(".show-more-item:hidden").slice(0).fadeIn();
+        if ($(".show-more-item:hidden").length < 1) $(this).fadeOut();
+    });
+    $(".show-less-btn").click(function (e) {
+        $(".show-more-item:hidden").slice(0).fadeIn();
+        if ($(".show-more-item:hidden").length < 1) $(this).fadeOut();
+    });
+});
+
+// $(function () {
+//     $("#testtext-1").show("slow");
+//     $("#testtext-2").hide();
+//     $("#testtext-3").hide();
+//     $("div.test-2").addClass("changeafter");
+//     $("div.test-3").addClass("changeafter");
+
+//     $("#test-1").click(function () {
+//         $("#testtext-1").show("slow");
+//         $("#testtext-2").hide();
+//         $("#testtext-3").hide();
+//         $("div.test-1").removeClass("changeafter");
+//         $("div.test-2").addClass("changeafter");
+//         $("div.test-3").addClass("changeafter");
+//     });
+//     $("#test-2").click(function () {
+//         $("#testtext-1").hide();
+//         $("#testtext-2").show("slow");
+//         $("#testtext-3").hide();
+//         $("div.test-2").removeClass("changeafter");
+//         $("div.test-1").addClass("changeafter");
+//         $("div.test-3").addClass("changeafter");
+//     });
+//     $("#test-3").click(function () {
+//         $("#testtext-1").hide();
+//         $("#testtext-2").hide();
+//         $("#testtext-3").show("slow");
+//         $("div.test-3").removeClass("changeafter");
+//         $("div.test-2").addClass("changeafter");
+//         $("div.test-1").addClass("changeafter");
+//     });
+// });
+
+// jQuery.
+$(function() {
+	// Reference the tab links.
+	const tabLinks = $('#tab-links li a');
+	
+	// Handle link clicks.
+	tabLinks.click(function(event) {
+		var $this = $(this);
+		
+		// Prevent default click behaviour.
+		event.preventDefault();
+		
+		// Remove the active class from the active link and section.
+		$('#tab-links a.active, section.active').removeClass('active');
+		
+		// Add the active class to the current link and corresponding section.
+		$this.addClass('active');
+		$($this.attr('href')).addClass('active');
+	});
+});
+
+  
