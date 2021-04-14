73d2eca4214a251b23356bb2084f567ba2f0aecf
SnehaBirodkar <snehabirodkar31@gmail.com>
2021-04-14 18:21:47 +0530

    testimonial reponsive

--- assets/script/script.js
+++ assets/script/script.js
@@ -28,10 +28,10 @@ $('.responsive').slick({
     responsive: [{
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 1,
+                slidesToScroll: 1,
                 infinite: true,
-                dots: true
+                dots: false
             }
         },
         {
